b5e6315388183b23911f93f2db41533b36a97129
1alishqa <1alishqa@gmail.com>
2020-10-25 01:02:20 +0600

    solved2: if-else

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -2,24 +2,35 @@ import random
 
 def is_positive(num):
     if num > 0:
-        return true
-    return false
+        return True
+    else:
+        return False
     # return true if num is positive, otherwise return false
 
 def is_even(num):
     if (num % 2 == 0):
-        return true
-    return false
+        return True
+    else:
+        return False
     # return true if num is even, otherwise return false
 
 def is_positive_and_even(num):
     if (is_positive(num) and is_even(num)):
-        return true
-    return false
+        return True
+    else:
+        return False
     # return true if num is positive and even, otherwise return false
 
 def is_positive_or_even(num):
     if (is_positive(num) or is_even(num)):
-        return true
-    return false
+        return True
+    else:
+        return False
     # return true if num is positive or even, otherwise return false
+
+# number = random.randint(-1000, 1000)
+# print(number)
+# print(is_positive(number))
+# print(is_even(number))
+# print(is_positive_and_even(number))
+# print(is_positive_or_even(number))
