049905b18d12715b106ee9a85ddfadf388052123
1alishqa <1alishqa@gmail.com>
2020-10-25 17:33:23 +0600

    solved: loops-while

--- loops-while/arrayWhileLoop.py
+++ loops-while/arrayWhileLoop.py
@@ -1,2 +1,10 @@
 def insert_squares(arr, num):
+    i = 1
+    while i < num + 1:
+        arr.append(i ** 2)
+        i = i + 1
+    return arr
     # add square of numbers from 1 to num to the list named arr and return list
+
+arr = [1, 2, 3]
+print(insert_squares(arr, 5))
