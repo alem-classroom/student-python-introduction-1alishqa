ef5cbbec1a22efa8c6c39d4d7a750fa0d56eaf11
1alishqa <1alishqa@gmail.com>
2020-11-17 17:24:01 +0600

    solved4: lists

--- lists/lists.py
+++ lists/lists.py
@@ -35,20 +35,20 @@ def reverse(list):
     return list
     # return reversed list
 
-print(size_of_list([1, 4, 'cat']))
-print(size_of_list([[5, 6], ['gym', 63]]), '\n')
+# print(size_of_list([1, 4, 'cat']))
+# print(size_of_list([[5, 6], ['gym', 63]]), '\n')
 
-print(add_elem_to_list([1, 2], 3))
-print(add_elem_to_list([70, 71, 72, 73], 'seventy four 74'), '\n')
+# print(add_elem_to_list([1, 2], 3))
+# print(add_elem_to_list([70, 71, 72, 73], 'seventy four 74'), '\n')
 
-print(delete_elem_from_list([0, 2, 6, 10, 13, 17, 6, 10, 20, 30, 15, 77], 90))
-print(delete_elem_from_list([7, 'cat', 9], -2), '\n')
+# print(delete_elem_from_list([0, 2, 6, 10, 13, 17, 6, 10, 20, 30, 15, 77], 90))
+# print(delete_elem_from_list([7, 'cat', 9], -2), '\n')
 
-print(count_elements_in_list([5, 7, 15, 7, 5, 5], 5))
-print(count_elements_in_list([5, 7, 15, 7, 5, 5], 55), '\n')
+# print(count_elements_in_list([5, 7, 15, 7, 5, 5], 5))
+# print(count_elements_in_list([5, 7, 15, 7, 5, 5], 55), '\n')
 
-print(sort_list([5, 4, 3, 2, 1, 0]))
-print(sort_list([15, 16, 17, 18]), '\n')
+# print(sort_list([5, 4, 3, 2, 1, 0]))
+# print(sort_list([15, 16, 17, 18]), '\n')
 
-print(reverse([0, 1, 2, 3, 4, 5]))
-print(reverse(['salam', 707, 104, 'aleikum']))
+# print(reverse([0, 1, 2, 3, 4, 5]))
+# print(reverse(['salam', 707, 104, 'aleikum']))
